8de7f03c6ffb829308c0ed9de4781883fe032c24
Florian Tönjes <skatepainter@yahoo.de>
2021-02-08 15:14:52 +0100

    Reducer working

--- src/app/reducers/affirmation.reducer.ts
+++ src/app/reducers/affirmation.reducer.ts
@@ -1,6 +1,10 @@
-import {createReducer, on} from '@ngrx/store';
+import {Action, createReducer, on} from '@ngrx/store';
 import {createAffirmation, updateAffirmation, deleteAffirmation} from '../actions/affirmation.actions';
 import {Affirmation} from '../shared/models/Affirmation';
+
+export interface State {
+  affirmations: Affirmation[];
+}
 
 export const initialState = {
   affirmations: [] as Affirmation[]
@@ -11,3 +15,7 @@ const affirmationReducer = createReducer(
   on(createAffirmation, (state, {affirmation}) => ({affirmations: [...state.affirmations, affirmation]}))
 );
 
+
+export function reducer(state: State | undefined, action: Action): State {
+  return affirmationReducer(state, action);
+}

32e3473bb9925dd16b02bcf4b7a1c5bf3e0ac1c3
Florian Tönjes <skatepainter@yahoo.de>
2021-02-08 14:51:25 +0100

    Created Affirmation.ts

---
+++ src/app/shared/models/Affirmation.ts
@@ -0,0 +1,11 @@
+export class Affirmation {
+  id: number;
+  title: string;
+  text: string;
+
+  constructor(id: number, title: string, text: string) {
+    this.id = id;
+    this.title = title;
+    this.text = text;
+  }
+}

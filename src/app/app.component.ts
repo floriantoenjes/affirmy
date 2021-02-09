bda7de0615b2885c6eebfff0a92ef381956a4cd9
Florian Tönjes <skatepainter@yahoo.de>
2021-02-09 20:55:13 +0100

    PouchDb working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as PouchDb from 'pouchdb-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'affirmy';
+
+  ngOnInit(): void {
+    const db = new PouchDB('affirmations');
+    db.put({
+      _id: new Date().toISOString(),
+      title: 'Title',
+      text: 'Text',
+    }).then();
+
+    console.log(db.allDocs({
+      include_docs: true, descending: true
+    }).then((result) => console.log(result)));
+  }
 }

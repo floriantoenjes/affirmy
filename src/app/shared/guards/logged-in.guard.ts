9bff83b0441cbf40ba5653795de92171d47ca010
Florian Tönjes <skatepainter@yahoo.de>
2021-02-21 09:23:19 +0100

    Logging out on token expiry now.

--- src/app/shared/guards/logged-in.guard.ts
+++ src/app/shared/guards/logged-in.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from '../services/auth.service';
+import {DateTime} from 'luxon';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,14 @@ export class LoggedInGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     const loggedIn = this.authService.isLoggedIn();
+    let jwt = this.authService.getJwt() as any;
+    if (jwt) {
+      jwt = this.authService.decodeJwt(jwt);
+      if (+jwt.exp < DateTime.local().toSeconds()) {
+        this.authService.logout();
+        return false;
+      }
+    }
 
     if (!loggedIn) {
       this.router.navigate(['/login']);

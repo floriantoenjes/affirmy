6a351f11cf39ebe7cf5c124c69a131b1075e4fa3
Florian Toenjes <skatepainter@yahoo.de>
2022-03-05 22:50:33 +0100

    Simplified some code

--- src/app/shared/models/Affirmation.ts
+++ src/app/shared/models/Affirmation.ts
@@ -23,8 +23,6 @@ export class Affirmation extends AffirmationDto{
     let schedule = null;
     if (this.scheduleModel?.scheduleType === ScheduleType.DAILY && !time && !days) {
       schedule = new DailySchedule(this.scheduleModel, (this.scheduleModel as DailySchedule).scheduleDays);
-    } else if (time && days) {
-      schedule = new DailySchedule(new ScheduleDto(ScheduleType.DAILY, this._id, time), days);
     } else if (!time || !days) {
       throw new Error('Time and days on first schedule needed!');
     } else {
@@ -35,31 +33,12 @@ export class Affirmation extends AffirmationDto{
     this.scheduleModel = schedule;
 
     return schedule.schedule();
-
-    /*let schedule = null;
-    if (!this.scheduleModel || this.scheduleModel.scheduleType !== ScheduleType.DAILY) {
-      if (!time || !days) {
-        throw new Error('Time and days on first schedule needed!');
-      }
-      schedule = new DailySchedule(new ScheduleDto(ScheduleType.DAILY, this._id, time), days);
-      this.scheduleModel = schedule;
-    } else {
-      schedule = new DailySchedule(this.scheduleModel, (this.scheduleModel as DailySchedule).scheduleDays);
-    }
-    this.scheduled = true;
-
-    console.log(schedule);
-
-    return schedule.schedule();
-    */
   }
 
   scheduleHourly(time?: string, hourlyInterval?: number): DateTime[] {
     let schedule = null;
     if (this.scheduleModel?.scheduleType === ScheduleType.HOURLY && !time && !hourlyInterval) {
       schedule = new HourlySchedule(this.scheduleModel, (this.scheduleModel as HourlySchedule).hourlyInterval);
-    } else if (time && hourlyInterval) {
-      schedule = new HourlySchedule(new ScheduleDto(ScheduleType.HOURLY, this._id, time), hourlyInterval);
     } else if (!time || !hourlyInterval) {
       throw new Error('Time and hourly interval on first schedule needed!');
     } else {
@@ -70,23 +49,6 @@ export class Affirmation extends AffirmationDto{
     this.scheduleModel = schedule;
 
     return schedule.schedule();
-
-    /*let schedule = null;
-    if (!this.scheduleModel || this.scheduleModel.scheduleType !== ScheduleType.HOURLY) {
-      if (!time || !hourlyInterval) {
-        throw new Error('Time and hourly interval on first schedule needed!');
-      }
-      schedule = new HourlySchedule(new ScheduleDto(ScheduleType.HOURLY, this._id, time), hourlyInterval);
-      this.scheduleModel = schedule;
-    } else {
-      schedule = new HourlySchedule(this.scheduleModel, (this.scheduleModel as HourlySchedule).hourlyInterval);
-    }
-    this.scheduled = true;
-
-    console.log(schedule);
-
-    return schedule.schedule();
-    */
   }
 
   cancelSchedule(): ScheduleDto | void {

058246f4b548f1a33013c08a4ded6db56d755e95
Florian Toenjes <skatepainter@yahoo.de>
2022-03-07 13:03:09 +0100

    Renamed type

--- src/app/shared/models/Notification.ts
+++ src/app/shared/models/Notification.ts
@@ -1,16 +1,16 @@
 import {DateTime} from 'luxon';
 import {ScheduleType} from './ScheduleDto';
 
-type every = 'year' | 'month' | 'two-weeks' | 'week' | 'day' | 'hour' | 'minute' | 'second' | undefined;
+type Every = 'year' | 'month' | 'two-weeks' | 'week' | 'day' | 'hour' | 'minute' | 'second' | undefined;
 
 export class Notification {
   type: ScheduleType;
   id: number;
   dateTime: DateTime;
-  every: every;
+  every: Every;
   count: number;
 
-  constructor(type: ScheduleType, id: number, dateTime: DateTime, every: every, count: number) {
+  constructor(type: ScheduleType, id: number, dateTime: DateTime, every: Every, count: number) {
     this.type = type;
     this.id = id;
     this.dateTime = dateTime;
